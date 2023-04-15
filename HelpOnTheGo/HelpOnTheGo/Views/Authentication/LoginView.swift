90a9402c4d97cf5f9e1d79be2a1eb1b2f046f430
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-15 11:41:53 -0400

    Implemented login view

--- HelpOnTheGo/HelpOnTheGo/Views/Authentication/LoginView.swift
+++ HelpOnTheGo/HelpOnTheGo/Views/Authentication/LoginView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            topBar
+            CustomAuthTextField(placeholder: "To get started enter your username", isSecureTxtField: false, text: $username)
+            CustomAuthTextField(placeholder: "password", isSecureTxtField: true, text: $password)
+            
+            loginButton
+            
+            Spacer(minLength: 0)
+        }
+    }
+    
+    private var topBar: some View {
+        HStack {
+            Button {
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Cancel")
+            }
+            .padding(.trailing, 105)
+
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .center)
+                .foregroundColor(.blue)
+
+            Spacer()
+        }
+        .padding()
+    }
+    
+    private var loginButton: some View {
+        VStack {
+            NavigationLink {
+                MainView()
+            } label: {
+                Capsule()
+                    .foregroundColor(.backgroundColor)
+                    .frame(width: 250, height: 50, alignment: .center)
+                    .overlay {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                    }
+            }
+
+
+        }.padding()
     }
 }
 
--- HelpOnTheGo/HelpOnTheGo/Views/Authentication/WelcomeView.swift
+++ HelpOnTheGo/HelpOnTheGo/Views/Authentication/WelcomeView.swift
@@ -9,7 +9,92 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.backgroundColor
+
+                VStack(alignment: .center, spacing: 2) {
+                    helpOnTheGoText
+                    Spacer()
+                    Image("TweetImage")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                    Spacer(minLength: 0)
+
+                    loginButtons
+                    
+                    divider
+
+                    alreadyHaveAnAccount
+                    
+                    Spacer(minLength: 0)
+                }
+                .padding(.top, 80)
+            }
+            .ignoresSafeArea(.all)
+            .navigationTitle("")
+            .toolbar(.hidden)
+        }
+    }
+
+    private var divider: some View {
+        HStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .opacity(0.3)
+                .frame(width: UIScreen.main.bounds.width * 0.35, height: 1)
+
+            Text("Or")
+                .foregroundColor(.white)
+
+            Rectangle()
+                .foregroundColor(.white)
+                .opacity(0.3)
+                .frame(width: UIScreen.main.bounds.width * 0.35, height: 1)
+        }
+    }
+
+    private var helpOnTheGoText: some View {
+        VStack(alignment: .center, spacing: 8) {
+            Text("Help On The Go!")
+                .foregroundColor(.white)
+                .fontWeight(.heavy)
+                .font(.system(size: 40))
+            Text("Empowering Connections, Strengthening Communities")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .fontWeight(.medium)
+        }
+    }
+
+    private var loginButtons: some View {
+        VStack(spacing: 15) {
+            AccountButtons(buttonType: .google) {
+                print("Signin with google")
+            }
+
+            AccountButtons(buttonType: .other) {
+                print("other sign up")
+            }
+        }.padding(8)
+    }
+
+    private var alreadyHaveAnAccount: some View {
+        VStack(spacing: 15) {
+            Text("Already have an account ?")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+            NavigationLink {
+                LoginView()
+            } label: {
+                AccountButtons(buttonType: .login) {
+                    print("login")
+                }
+            }
+
+            
+        }
     }
 }
 
