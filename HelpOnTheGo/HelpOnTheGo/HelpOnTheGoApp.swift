d8c001b2782ab1e963656f75def3d606bd2ece54
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-15 09:03:08 -0400

    init commit

---
+++ HelpOnTheGo/HelpOnTheGo/HelpOnTheGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpOnTheGoApp.swift
+//  HelpOnTheGo
+//
+//  Created by Matthew Fraser on 2023-04-15.
+//
+
+import SwiftUI
+
+@main
+struct HelpOnTheGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
