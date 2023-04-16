5b5d5629356bac094f78c7d23677b4ec95a48aee
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2023-04-16 00:04:37 -0400

    Develop 0.2.0 (#3)
    * Implemented login view
    
    * MapView
    
    * register view
    
    * implement message view
    
    * Message progress
    
    - bug fixes
    
    * Code cleanup
    
    * Messaging progress
    
    * Chat Message Progress
    
    * MESSAGING THAT WORKS

--- HelpOnTheGo/HelpOnTheGo/Views/Messaging/MessageListView.swift
+++ HelpOnTheGo/HelpOnTheGo/Views/Messaging/MessageListView.swift
@@ -12,48 +12,13 @@ struct MessageListView: View {
     @State var selectedMessage: Message? = nil
     @State var toggleMessageChat: Bool = false
     @State var shouldShowNewMessage: Bool = false
+    @State var chatUser: ChatUser?
     @ObservedObject private var vm = MessageViewModel()
 
     var body: some View {
         NavigationView {
             ScrollView(.vertical) {
-                VStack {
-                    Text("Messages")
-                        .foregroundColor(.backgroundColor)
-                        .font(.largeTitle)
-                    HStack {
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .frame(width: 35, height: 35)
-
-                        VStack(alignment: .leading) {
-                            Text("Email: \(vm.chatUser?.email ?? "")")
-                                .fontWeight(.bold)
-                            HStack {
-                                Text("online")
-                                    .font(.system(size: 12))
-                                    .foregroundColor(.black)
-                                Circle()
-                                    .foregroundColor(.green)
-                                    .frame(width: 15, height: 15)
-                            }
-                        }
-
-                        Spacer(minLength: 0)
-
-                        Image(systemName: "message")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .foregroundColor(.blue)
-                            .onTapGesture {
-                                self.shouldShowNewMessage.toggle()
-                            }
-                            .fullScreenCover(isPresented: $shouldShowNewMessage, onDismiss: nil) {
-                                NewMessageView()
-                            }
-                        
-                    }.padding()
-                }
+                header
 
                 Divider()
 
@@ -63,14 +28,60 @@ struct MessageListView: View {
                             self.selectedMessage = message
                             
                             withAnimation {
-                                self.toggleMessageChat = true
+                                self.toggleMessageChat.toggle()
                             }
                         }
                     Divider()
                 }
 
-                NavigationLink(destination: MessagePersonView(), isActive: $toggleMessageChat, label: { EmptyView() } )
+                NavigationLink(
+                    destination: MessagePersonView(chatUser: self.chatUser),
+                    isActive: $toggleMessageChat, label: { EmptyView() }
+                )
             }
+        }
+    }
+    
+    private var header: some View {
+        VStack {
+            Text("Messages")
+                .foregroundColor(.backgroundColor)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 35, height: 35)
+
+                VStack(alignment: .leading) {
+                    Text("Email: \(vm.chatUser?.email ?? "")")
+                        .fontWeight(.bold)
+                    HStack {
+                        Text("online")
+                            .font(.system(size: 12))
+                            .foregroundColor(.black)
+                        Circle()
+                            .foregroundColor(.green)
+                            .frame(width: 15, height: 15)
+                    }
+                }
+
+                Spacer(minLength: 0)
+
+                Image(systemName: "message")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.blue)
+                    .onTapGesture {
+                        self.shouldShowNewMessage.toggle()
+                    }
+                    .fullScreenCover(isPresented: $shouldShowNewMessage, onDismiss: nil) {
+                        NewMessageView(didSelectNewUser: { user in
+                            self.toggleMessageChat.toggle()
+                            self.chatUser = user
+                        })
+                    }
+                
+            }.padding()
         }
     }
 }
--- HelpOnTheGo/HelpOnTheGo/Views/Messaging/NewMessageView.swift
+++ HelpOnTheGo/HelpOnTheGo/Views/Messaging/NewMessageView.swift
@@ -7,13 +7,67 @@
 
 import SwiftUI
 
+class CreateNewMessageViewModel: ObservableObject {
+    @Published var users = [ChatUser]()
+    @Published var errorMessage = ""
+
+    init() {
+        fetchAllUsers()
+    }
+
+    private func fetchAllUsers() {
+        FirebaseManager.shared.firestore.collection("users")
+            .getDocuments { snapshot, error in
+                if let error = error {
+                    self.errorMessage = "failed to fetch users: \(error)"
+                    print("failed to fetch users: \(error)")
+                }
+                
+                snapshot?.documents.forEach({ snapshot in
+                    let data = snapshot.data()
+
+                    self.users.append(.init(data: data))
+                })
+            }
+    }
+}
+
 struct NewMessageView: View {
     @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var vm = CreateNewMessageViewModel()
+
+    let didSelectNewUser: (ChatUser) -> ()
 
     var body: some View {
         NavigationView {
             ScrollView(.vertical) {
-                
+                Text(vm.errorMessage)
+
+                ForEach(vm.users) { user in
+                    Button {
+                        self.presentationMode.wrappedValue.dismiss()
+                        didSelectNewUser(user)
+                    } label: {
+                        HStack(spacing: 16) {
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.backgroundColor)
+                                .font(.system(size: 20))
+                                .padding()
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color.backgroundColor, lineWidth: 1)
+                                }
+                            Text(user.email)
+                                .foregroundColor(.primary)
+                            
+                            Spacer()
+                        }.padding(.horizontal)
+                        
+                        Divider()
+                            .padding(.vertical, 8)
+                    }
+
+                }
             }
             .navigationTitle("New Message")
             .toolbar {
@@ -30,8 +84,8 @@ struct NewMessageView: View {
     }
 }
 
-struct NewMessageView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewMessageView()
-    }
-}
+//struct NewMessageView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        // NewMessageView()
+//    }
+//}
